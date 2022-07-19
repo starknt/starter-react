a1f7dcf2024bad53a4c5381a34621e27ba8eccbc
starknt <1431880400@qq.com>
2022-07-19 13:37:09 +0800

    fix: vite config type

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import unocss from 'unocss/vite'
 import autoImport from 'unplugin-auto-import/vite'
 
-export default ({
+export default defineConfig({
   plugins: [
     react(),
     unocss({
